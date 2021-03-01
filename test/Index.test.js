68fd123efdc2b2193503e8996a006f4b151fee8d
leighgordy <leighgordy@gmail.com>
2021-03-01 15:47:52 +0000

    Changed index test to check that the correct module is exported

--- test/Index.test.js
+++ test/Index.test.js
@@ -1,7 +1,8 @@
 import Index from '../lib/Index';
+import App from '../lib/App';
 
 describe('Index.js', () => {
-  test('Test module exports objects', () => {
-    expect(Index).not.toBeNull();
+  test('Test module exports correct objects', () => {
+    expect(Index).toBe(App);
   });
 });
